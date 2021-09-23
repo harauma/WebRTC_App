33bb6580f04a716fc8d2706d666d976496f6c974
harauma <fot39hara@gmail.com>
2021-09-23 11:46:25 +0900

    create video component

---
+++ src/components/Video.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Video = ({ isLocal, name, videoRef }) => {
+  return (
+    <div>
+      <video
+        autoPlay
+        muted={ isLocal }
+        ref={ videoRef }
+      />
+      <div>{ name }</div>
+    </div>
+  );
+};
+
+export default Video;
